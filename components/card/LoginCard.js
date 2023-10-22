8c0cea4c16d560e86035d86eefec779732a69f36
maulana2710 <vickymaulana2707@gmail.com>
2023-10-22 17:28:50 +0700

    Co-authored-by: Deemazter07 <Deemazter07@users.noreply.github.com>

--- components/card/LoginCard.js
+++ components/card/LoginCard.js
@@ -1,7 +1,9 @@
-export default function LoginCard({ closeLogin }) {
+"use client"
+
+export default function LoginCard({ closeLogin, cekInput, handleChangeInput, handleSubmitInput }) {
   return (
     <>
-      <div class="w-screen max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-screen max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         <div className="grid text-end">
           <div>
             <button
@@ -12,78 +14,81 @@ export default function LoginCard({ closeLogin }) {
             </button>
           </div>
         </div>
-        <form class="space-y-6" action="#">
-          <h5 class="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
+        <form className="space-y-6" action="#" onSubmit={handleSubmitInput}>
+          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+            LOGIN
           </h5>
           <div>
             <label
-              for="email"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Your email
             </label>
             <input
               type="email"
               name="email"
+              value={cekInput.email}
+              onChange={handleChangeInput}
               id="email"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               placeholder="name@company.com"
               required
             />
           </div>
           <div>
             <label
-              for="password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Your password
             </label>
             <input
               type="password"
               name="password"
+              value={cekInput.password}
+              onChange={handleChangeInput}
               id="password"
               placeholder="••••••••"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               required
             />
           </div>
-          <div class="flex items-start">
-            <div class="flex items-start">
-              <div class="flex items-center h-5">
+          <div className="flex items-start">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
                 <input
                   id="remember"
                   type="checkbox"
                   value=""
-                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                  required
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
                 />
               </div>
               <label
-                for="remember"
-                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                htmlFor="remember"
+                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Remember me
               </label>
             </div>
             <a
               href="#"
-              class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+              className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
             >
               Lost Password?
             </a>
           </div>
           <button
             type="submit"
-            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Login to your account
           </button>
-          <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+          <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
             Not registered?{" "}
             <a
               href="#"
-              class="text-blue-700 hover:underline dark:text-blue-500"
+              className="text-blue-700 hover:underline dark:text-blue-500"
             >
               Create account
             </a>
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,9 +1,36 @@
+"use client"
 import LoginCard from "@/components/card/LoginCard";
+import axios from "axios";
+import React from "react";
 
 export default function Login({ closeLogin }) {
+    const [input, setInput] = React.useState({})
+    // console.log(input);
+const host = env
+
+    const handleChangeInput = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInput((values) => ({ ...values, [name]: value }));
+      };
+    
+      const handleSubmitInput = async (event) => {
+        event.preventDefault();
+        console.log(`isi input Email: ${input.email}`);
+        try {
+          const { data } = await axios.post("https://7fa8-2001-448a-2034-30bc-99ca-58d3-2030-ac4b.ngrok-free.app/auth/login", {
+            emailPhone: input.email,
+            password: input.password
+          });
+          console.log(`data`, data);
+        } catch (error) {
+          console.log("login gagal", error);
+          throw error;
+        }
+      }
     return(
         <div className="absolute translate-y-1/2 translate-x-1/2 top-0 right-1/2">
-        <LoginCard closeLogin={closeLogin}/>
+        <LoginCard closeLogin={closeLogin} cekInput={setInput} handleChangeInput={handleChangeInput} handleSubmitInput={handleSubmitInput} />
         </div>
     )
 }
